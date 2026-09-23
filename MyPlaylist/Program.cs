0ec78de54041b1089255439801e79b4b457115e0
ybabingo-hash <ybabingo@gmail.com>
2026-09-23 23:33:23 +0500

    Add first track

---
+++ MyPlaylist/Program.cs
@@ -0,0 +1,7 @@
+﻿Track track = new Track();
+
+track.Name = "B.O.M.J.";
+track.Artist = "DJ Ulitka";
+track.DurationSeconds = 120;
+
+Console.WriteLine($"New track {track.Name} was dropped by {track.Artist}, length of song is {track.DurationSeconds} seconds!");
---
+++ MyPlaylist/Track.cs
@@ -0,0 +1,6 @@
+public class Track
+{
+    public string Name { get; set; } = "";
+    public string Artist { get; set; } = "";
+    public int DurationSeconds { get; set; }
+}
